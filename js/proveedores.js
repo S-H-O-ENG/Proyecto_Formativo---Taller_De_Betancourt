5c6909b65c9ea90851fde3068e12562ee4ef2013
matatis31-dot <matatis31@gmail.com>
2026-06-23 15:47:52 -0500

    actualizacion

---
+++ js/proveedores.js
@@ -0,0 +1,16 @@
+new DataTable('tablaProveedores', {
+    language: {
+        search: "🔍 Buscar:",
+        lengthMenu: "Mostrar _MENU_ registros",
+        info: "Mostrando _START_ a _END_ de _TOTAL_ proveedores",
+        emptyTable: "No hay proveedores registrados",
+        zeroRecords: "No se encontraron resultados",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    },
+    pageLength: 5
+})
