35b8bc365505a837ca49e41da1f5be2bfb535004
S-H-O-ENG <sebastianherreraobando.alone@gmail.com>
2026-09-23 13:23:06 -0500

    responsivo

--- Taller De Betancourt/src/main.jsx
+++ Taller De Betancourt/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import InicioJefe from './pages/InicioJefe.jsx'
+
 import App from './App.jsx';
+import Pedidos from './pages/Pedidos.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App   />
+    <App  />
   </StrictMode>,
 )
